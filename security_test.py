fa398ba6001538c66f226a088f7203de03ff9656
Yammany Rollins <yammanyrollins@Yammanys-MacBook-Pro.local>
2026-02-10 12:37:28 -0500

    Fixed hardcoded password vulnerability

--- security_test.py
+++ security_test.py
@@ -2,7 +2,7 @@ import os
 
 def verify_password(input_password):
     # This is a hardcoded password (Codacy should flag this as a security risk)
-    if input_password == "admin123":
+    if input_password == os.environ.get("ADMIN_PASSWORD"):
         print("Access Granted")
     else:
         print("Access Denied")
